e9dd44072cd800c685b58692ec2d8e6d70c2e003
Carla Mariana <carlamab2808@outlook.com>
2024-11-03 00:26:30 -0300

    version 16 angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
-import { MegaMenuModule } from 'primeng/megamenu';
-import { MenuModule } from 'primeng/menu';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HomeComponent, MegaMenuModule, MenuModule, CascadeSelectModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'owlTechHub';
